67854f40cfcb49de6b989d51c2ab12e5b9594e3f
Dannelysbeth <danielaf@onet.pl>
2025-10-20 20:30:42 +0200

    added location entity

---
+++ src/main/java/digital/queens/hackathon/AIReconCommand/model/Location.java
@@ -0,0 +1,27 @@
+package digital.queens.hackathon.AIReconCommand.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "drone_locations")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Location {
+    @Id
+    private Long id;
+
+    private String name;
+
+    private double latitude;
+
+    private double longitude;
+
+}
