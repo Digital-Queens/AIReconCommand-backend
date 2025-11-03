29f3761ce07a4c72177837a4772b34c4ceff0854
Dannelysbeth <danielaf@onet.pl>
2025-11-03 23:51:53 +0100

    added date to Full report

--- src/main/java/digital/queens/hackathon/AIReconCommand/model/dto/FullReport.java
+++ src/main/java/digital/queens/hackathon/AIReconCommand/model/dto/FullReport.java
@@ -3,10 +3,12 @@ package digital.queens.hackathon.AIReconCommand.model.dto;
 import lombok.Data;
 
 import java.time.LocalTime;
+import java.util.Date;
 
 @Data
 public class FullReport {
 
+    Date date;
     String description;
     LocalTime time;
 }
