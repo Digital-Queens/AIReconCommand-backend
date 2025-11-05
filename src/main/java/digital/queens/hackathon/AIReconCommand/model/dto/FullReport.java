53a6bb1ac4f8bf7b60fe02f0f820fc3c1144de56
Dannelysbeth <danielaf@onet.pl>
2025-11-05 23:27:50 +0100

    added date to Full report as entity

--- src/main/java/digital/queens/hackathon/AIReconCommand/model/dto/FullReport.java
+++ src/main/java/digital/queens/hackathon/AIReconCommand/model/dto/FullReport.java
@@ -1,11 +1,13 @@
 package digital.queens.hackathon.AIReconCommand.model.dto;
 
+import jakarta.persistence.Entity;
 import lombok.Data;
 
 import java.time.LocalTime;
 import java.util.Date;
 
 @Data
+@Entity
 public class FullReport {
 
     Date date;
